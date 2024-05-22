3363b989ba8e1a1d26417effacdef659b527534f
Patrick_Audley <paudley@blackcatinformatics.ca>
2024-05-21 22:35:33 -0700

    (qa) update for newer and more golangci-lint checks

--- color.go
+++ color.go
@@ -1,6 +1,4 @@
 // Copyright (C) 2022, 2023, 2024 by Blackcat Informatics® Inc.
-//
-// nolint: varnamelen,revive
 package colorout
 
 import (
@@ -18,21 +16,21 @@ var normal = spew.ConfigState{
 	SortKeys: true,
 }
 
-func Sdump(i interface{}) string {
+func Sdump(i any) string {
 	return normal.Sdump(i)
 }
 
 // SdumpColored dumps a variable to a string with colored syntax highlighting.
-func SdumpColored(i ...interface{}) string {
+func SdumpColored(i ...any) string {
 	s := normal.Sdump(i...)
 
-	var b bytes.Buffer
+	var buffer bytes.Buffer
 
-	if err := quick.Highlight(&b, s, "go", "terminal16m", "dracula"); err != nil {
+	if err := quick.Highlight(&buffer, s, "go", "terminal16m", "dracula"); err != nil {
 		return s
 	}
 
-	return b.String()
+	return buffer.String()
 }
 
 var simple = &spew.ConfigState{
@@ -44,62 +42,69 @@ var simple = &spew.ConfigState{
 
 var simpleStripRe = regexp.MustCompile(`\(len=\d+\)\s?`)
 
-func SimpleColorString(language string, s string) string {
-	sR := simpleStripRe.ReplaceAllString(s, "")
+func SimpleColorString(language, str string) string {
+	strReplaced := simpleStripRe.ReplaceAllString(str, "")
 
-	var b bytes.Buffer
+	var buffer bytes.Buffer
 
-	if err := quick.Highlight(&b, sR, language, "terminal16m", "dracula"); err != nil {
-		return s
+	if err := quick.Highlight(
+		&buffer,
+		strReplaced,
+		language,
+		"terminal16m",
+		"dracula",
+	); err != nil {
+		return str
 	}
 
-	return b.String()
+	return buffer.String()
 }
 
 // SdumpColoredSimple dumps a variable to a string with colored syntax highlighting.
-func SdumpColorSimple(i ...interface{}) string {
+func SdumpColorSimple(i ...any) string {
 	return SimpleColorString("go", simple.Sdump(i...))
 }
 
 // SdumpColoredJSON dumps a variable to a string with colored syntax highlighting.
-func SdumpColorJSON(m json.Marshaler) string {
-	j, err := m.MarshalJSON()
+func SdumpColorJSON(object json.Marshaler) string {
+	jsonStr, err := object.MarshalJSON()
 	if err != nil {
 		return "ERROR ENCODING JSON"
 	}
 
-	return SimpleColorString("json", string(j))
+	return SimpleColorString("json", string(jsonStr))
 }
 
-var White = color.New(color.FgWhite, color.Bold)
-var Red = color.New(color.FgRed, color.Bold)
-var Orange = color.New(color.FgYellow)
-var Yellow = color.New(color.FgYellow, color.Bold)
-var Green = color.New(color.FgGreen, color.Bold)
-var Cyan = color.New(color.FgCyan, color.Bold)
-var Blue = color.New(color.FgBlue, color.Bold)
-var Magenta = color.New(color.FgMagenta)
-var Grey = color.New(color.FgWhite)
-var WhiteOnMagenta = color.New(color.BgMagenta, color.Bold, color.FgWhite)
-var WhiteOnBlue = color.New(color.BgBlue, color.FgWhite, color.Bold)
-var WhiteOnGreen = color.New(color.BgGreen, color.Bold, color.FgWhite)
-var WhiteOnCyan = color.New(color.BgCyan, color.Bold, color.FgWhite)
-var WhiteOnRed = color.New(color.BgRed, color.Bold, color.FgWhite)
-var BlackOnYellow = color.New(color.BgHiYellow, color.FgBlack)
+var (
+	White          = color.New(color.FgWhite, color.Bold)
+	Red            = color.New(color.FgRed, color.Bold)
+	Orange         = color.New(color.FgYellow)
+	Yellow         = color.New(color.FgYellow, color.Bold)
+	Green          = color.New(color.FgGreen, color.Bold)
+	Cyan           = color.New(color.FgCyan, color.Bold)
+	Blue           = color.New(color.FgBlue, color.Bold)
+	Magenta        = color.New(color.FgMagenta)
+	Grey           = color.New(color.FgWhite)
+	WhiteOnMagenta = color.New(color.BgMagenta, color.Bold, color.FgWhite)
+	WhiteOnBlue    = color.New(color.BgBlue, color.FgWhite, color.Bold)
+	WhiteOnGreen   = color.New(color.BgGreen, color.Bold, color.FgWhite)
+	WhiteOnCyan    = color.New(color.BgCyan, color.Bold, color.FgWhite)
+	WhiteOnRed     = color.New(color.BgRed, color.Bold, color.FgWhite)
+	BlackOnYellow  = color.New(color.BgHiYellow, color.FgBlack)
+)
 
-func Dump(name string, i ...interface{}) {
+func Dump(name string, object ...any) {
 	color.NoColor = false
 	title := White.SprintfFunc()
 	top := Yellow.SprintfFunc()
 	bottom := Blue.SprintfFunc()
 
-	fmt.Printf("\n%s %s %s\n",
-		top(">>>>>>>>------------------["),
-		title(name),
-		top("]--------------------------"))
-	fmt.Print(SdumpColored(i...))
-	fmt.Printf("%s %s %s\n",
-		bottom("<<<<<<<<------------------["),
-		title(name),
-		bottom("]--------------------------"))
+	fmt.Print(
+		top(">>>>>>>>------------------[ ") +
+			title(name) +
+			top(" ]--------------------------\n") +
+			SdumpColored(object...) +
+			bottom("\n<<<<<<<<------------------[ ") +
+			title(name) +
+			bottom(" ]--------------------------\n"))
 }
